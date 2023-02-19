74649d4359fb290404353f9adf1e0ec4f0fae09c
Nagoor <nagoorvl@gmail.com>
2023-02-19 22:31:32 +0530

    Testng.xml Execution added

--- src/test/java/com/swarpa/runner/TestNgRunner.java
+++ src/test/java/com/swarpa/runner/TestNgRunner.java
@@ -17,7 +17,7 @@ import org.testng.annotations.DataProvider;
 )
 public class TestNgRunner extends AbstractTestNGCucumberTests {
     @Override
-    @DataProvider(parallel = false)
+    @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
